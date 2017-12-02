2ab6e98f779a6f7bdbc8e97b451e611a1c8ba652
twoflypig <huangxjlovel@gmail.com>
2017-12-02 20:31:03 +0800

    there are erros in alligned

--- model/model_add_aligned.py
+++ model/model_add_aligned.py
@@ -12,14 +12,14 @@ class model(object):
 
     def MultiBiRNN(self,inputs,hidden_units,num_layers,sequence_length,dropout_output,name):
 
-        data_in = inputs
+        output = inputs
 
         for i in range(num_layers):
 
             with  tf.variable_scope( name +str(i), reuse=False) as scope:
                 
                 if dropout_output < 1:
-                    output = tf.nn.dropout(data_in,dropout_output) 
+                    output = tf.nn.dropout(output,dropout_output) 
 
                 forward_cell = tf.contrib.rnn.LSTMCell(hidden_units)
                 backward_cell = tf.contrib.rnn.LSTMCell(hidden_units)
@@ -27,29 +27,89 @@ class model(object):
                 output, _states = tf.nn.bidirectional_dynamic_rnn(forward_cell, backward_cell, output,
                                                 sequence_length=sequence_length, time_major=True,dtype = tf.float32)
                 output =  tf.concat(output, -1)
-            print("{} layer {} : keep_pro".format(name,i,dropout_output))
+            print("{} layer {} : keep_pro:{}".format(name,i,dropout_output))
 
         if dropout_output < 1:
             output = tf.nn.dropout(output,dropout_output) 
 
         return output
+    def SeqAtten(self,martrix_passage,matrix_query,martrix_passage_length,matrix_query_length,name):
+        """
+            func: Given sequences X and Y, match sequence Y to each element in X.
+            input martrix_passage: length1,batch_size,feature
+                  matrix_query   : length2,batch_size,feature
+                  martrix_passage_length : batch_size,
+                  matrix_query_length :batch_size
+            return aligned_question_embeding: batch_size,length1,feature
+        """
+        with  tf.variable_scope( name , reuse=False) as scope:
+            alph_passage = tf.layers.dense(martrix_passage,martrix_passage.get_shape().as_list()[2],kernel_initializer =tf.random_uniform_initializer(-0.5, 0.5, seed=12),activation=tf.nn.relu,name='aligned_dense',reuse=None)
+            # shape of alph_question is [?,20,1]
+            alph_question = tf.layers.dense(matrix_query ,matrix_query.get_shape().as_list()[2],kernel_initializer =tf.random_uniform_initializer(-0.5, 0.5, seed=12),activation=tf.nn.relu,name='aligned_dense',reuse=True)
+            # result:[batch_size,? ,?] the result of matmul between [20,?,1] and [20,1,?]
+            # so dimension = 1 means passage pi , and simension =2 means question qi
+            batch_martix = tf.matmul( tf.transpose(alph_passage,[1,0,2]) , tf.transpose(alph_question,[1,2,0]) )
 
-    # def bilayer(self, inputs,layer_index,sequence_length , name):
+            # input query_sequence_length : [batch_size,]
+            # input passage_sequence_length : [batch_size,]
+            # the result of sequence_mask shape: batch,passage_length,query_length
+            # returned masked shape [batch,passage_length,query_length]
+            # sequence_mask=  tf.tile(tf.expand_dims(tf.sequence_mask(matrix_query_length),axis=1),[1,tf.reduce_max( tf.reshape(martrix_passage_length,(-1,1))),1])
 
-    #     output = inputs
-    #     with  tf.variable_scope( name +str(layer_index), reuse=False) as scope:
+            # sequence_ones = tf.ones_like(sequence_mask,dtype=tf.float32)
 
-    #         if self.config.keep_pro < 1:
-    #             output = tf.nn.dropout(output,self.config.keep_pro) 
-    #             print("{} rnn layer{} input,keep_pro:{}".format(name,layer_index,self.config.keep_pro)) 
+            # sequence_infi = sequence_ones * (-float('inf'))
 
-    #         forward_cell = tf.contrib.rnn.GRUCell(self.num_units)
-    #         backward_cell = tf.contrib.rnn.GRUCell(self.num_units)
-    #         output, _states = tf.nn.bidirectional_dynamic_rnn(forward_cell, backward_cell, output,
-    #                                                           sequence_length=sequence_length, time_major=True,dtype = tf.float32)
-    #         output =  tf.concat(output, -1)
+            # masked = tf.where(sequence_mask,batch_martix,sequence_infi)
+            # apply softmax ,dim default is -1 ,operates on the last dimension the batch_martix
+            #shape is [batch_size,?,?]
+            batch_softmax_martix = tf.nn.softmax(batch_martix)
+            #[20,?,200] =  [20,?,?] x [20,?,200]
+            aligned_question_embeding=  tf.matmul(batch_softmax_martix, tf.transpose(matrix_query,[1,0,2]) )    
+            print("building SeqAtten:{}".format(name))
+            return aligned_question_embeding
+    def bilineaAtten(self,matrix_passage,query,name):
+
+        with  tf.variable_scope( name , reuse=False) as scope:
+            bb_W =  tf.Variable(tf.truncated_normal([matrix_passage.shape.as_list()[2],query.shape.as_list()[0]], stddev=0.1))
             
-    #     return output
+            bb_W = tf.tile( tf.expand_dims(bb_W,dim=0),[self.batch_size,1,1])
+            bb_W_q = tf.matmul(bb_W, tf.expand_dims(tf.transpose(query,[1,0]),dim=-1))
+            bb_p = tf.transpose(matrix_passage,[1,0,2])
+            bb_pwq = tf.matmul(bb_p,bb_W_q)
+            p_W_q = tf.squeeze(bb_pwq,axis=2)
+
+            tf.summary.histogram("bilineaAtten"+name,bb_W)
+            print("building bilineaAtten:{}".format(name))
+            return p_W_q
+    def  SelfAtten(self,matrix,length,name):
+        """
+            func: Given sequences X , match attention to self.
+            input matrix: length1,batch_size,feature
+                  length   : batch_size,
+ 
+            return aligned_question_embeding: feature,batch_size
+        """
+        with tf.name_scope(name) as scope:
+
+            W = tf.Variable(tf.truncated_normal([matrix.get_shape().as_list()[2],1], stddev=0.1))
+
+            tile_w = tf.tile(tf.expand_dims(W,axis = 0),[self.batch_size,1,1])
+
+            weight_martix = tf.matmul(  tf.transpose(matrix,[1,0,2]),tile_w)
+
+            sequence_masked  =  tf.expand_dims(tf.sequence_mask(length),axis=2)
+            sequence_ones_1 = tf.ones_like(sequence_masked,dtype=tf.float32)
+            sequence_infi_1 = sequence_ones_1 * (-float('inf'))
+
+            weight_martix = tf.nn.softmax(tf.where(sequence_masked,weight_martix,sequence_infi_1),dim=1)
+
+            final_query = tf.reduce_sum( tf.transpose(matrix,[0,2,1]) *  tf.transpose(weight_martix,[1,2,0]) ,axis =0)
+
+            tf.summary.histogram("SelfAttenW"+name,W)
+
+            print("building SelfAtten:{}".format(name))
+        return final_query
     def build_model(self):
         # passage
         # as time-major
@@ -99,30 +159,10 @@ class model(object):
             # set global_step
             self.global_step = tf.Variable(0, trainable=False)
 
-        """
-        需要对输入向量增加三个特征，第一个是与问题对齐的特征
-        """
-        with tf.variable_scope('alligned'):
-            # tf.layers.dense operates on the last
-            # shape of alph_passage is [?,20,1]
-            # layerW = tf.get_variable('denserlayerW',[.get_shape().as_list()[2] , 200],
-            #                            initializer=,
-            #                             dtype=tf.float32)
-            # ,kernel_initializer =tf.random_uniform_initializer(-0.5, 0.5, seed=12)
-            alph_passage = tf.layers.dense(passage_inputs_embedded,200,activation=tf.nn.relu,name='aligned_dense',reuse=None)
-            # shape of alph_question is [?,20,1]
-            alph_question = tf.layers.dense(query_inputs_embedded ,200,activation=tf.nn.relu,name='aligned_dense',reuse=True)
-            # result:[batch_size,? ,?] the result of matmul between [20,?,1] and [20,1,?]
-            # so dimension = 1 means passage pi , and simension =2 means question qi
-            batch_martix = tf.matmul( tf.transpose(alph_passage,[1,0,2]) , tf.transpose(alph_question,[1,2,0]) )
-            # apply softmax ,dim default is -1 ,operates on the last dimension the batch_martix
-            #shape is [batch_size,?,?]
-            batch_softmax_martix = tf.nn.softmax(batch_martix)
 
-            aligned_question_embeding=  tf.matmul(batch_softmax_martix, tf.transpose(query_inputs_embedded,[1,0,2]) )
+        aligned_question_embeding= self.SeqAtten(passage_inputs_embedded,query_inputs_embedded,self.passage_sequence_length,
+                                            self.query_sequence_length,"q_p_alligned")
 
-
-            # for debuging
 
         with tf.name_scope("passage_rnn") as scope:
 
@@ -148,22 +188,8 @@ class model(object):
                                             name= "passage")
 
             
-            #passage_outputs =data_in
-            passage_shape = passage_outputs.get_shape().as_list()
-            
 
         with tf.name_scope("question_rnn") as scope:
-            with tf.variable_scope('w'):
-                # W shape is [1,1,400]
-                W = tf.Variable(tf.truncated_normal([passage_shape[2],1], stddev=0.1))
-
-            # data_in = query_inputs_embedded
-            # for i in range(self.config.num_layer):
-            #     data_in = self.bilayer(data_in,i,self.query_sequence_length,"query")
-
-            # if self.config.keep_pro < 1:
-            #     data_in = tf.nn.dropout(data_in,self.config.keep_pro) 
-            #     print("query rnn layer output,keep_pro:{}".format(self.config.keep_pro)) 
 
             question_outputs =  self.MultiBiRNN(inputs=query_inputs_embedded,
                                 hidden_units =self.num_units,
@@ -171,48 +197,15 @@ class model(object):
                                 sequence_length = self.query_sequence_length,
                                 dropout_output =self.config.keep_pro ,
                                 name= "query")
+            final_query = self.SelfAtten(question_outputs,self.query_sequence_length,name="self-atten")
 
-
-            #<tf.Tensor 'concat:0' shape=(?, self.batch_size, hidden_units*2) dtype=float32>
-            question_outputs  = tf.concat(question_outputs, -1)      
-            # a list of [?,400],len is self.batch_size
-            question_outputs_unstack  = tf.unstack(question_outputs,axis=1)
-            # a list of [?,1] , len is self.batch_size
-            result_b =  [tf.nn.softmax(tf.matmul(item ,W),dim=0) for item in question_outputs_unstack]
-            # stack of question_outputs_unstack shape is [?,400,self.batch_size]
-            question_stakced  =tf.stack(question_outputs_unstack,-1)
-            # stack of result_b, shape is [?,1,self.batch_size]
-            question_weighted  = tf.stack(result_b,-1)
-            # final_queryshape is [400,self.batch_size]
-            # question_stakced *  question_weighted shape=(?, 400, 6)
-            final_query = tf.reduce_sum(question_stakced *  question_weighted ,axis =0)
         with tf.name_scope('g') as scope:
 
-            passage_outputs_unstack =  tf.unstack(passage_outputs,axis =1)
 
-            with tf.name_scope("pre_q_start") as scope:
-                Ws =  tf.Variable(tf.truncated_normal([passage_outputs.shape.as_list()[2],final_query.shape.as_list()[0]], stddev=0.1,seed=321))
-                W_q = tf.matmul(Ws,final_query)
-                W_q = tf.reshape(W_q , shape=[tf.shape(W_q)[0],1,self.batch_size])
-                # self.p_W_q shape is [?,self.batch_size]
-                W_q_unstacked = tf.unstack(W_q,axis =2)
+                self.p_W_q =  self.bilineaAtten(passage_outputs,final_query,"pre_start")
 
-                self.p_W_q = []
-                for i in range(self.batch_size):
-                    self.p_W_q.append(tf.matmul(passage_outputs_unstack[i] ,W_q_unstacked[i]))
-                self.p_W_q = tf.concat(self.p_W_q,axis= -1)
-                self.p_W_q = tf.transpose(self.p_W_q ,[1,0])
 
-            with tf.name_scope("pre_q_end") as scope:
-                We =  tf.Variable(tf.truncated_normal([passage_outputs.shape.as_list()[2],final_query.shape.as_list()[0]], stddev=0.1,seed=123))
-                We_q = tf.matmul(We,final_query)
-                We_q = tf.reshape(We_q , shape=[tf.shape(We_q)[0],1,self.batch_size])
-                We_q_unstacked = tf.unstack(We_q,axis =2)
-                self.p_We_q = []
-                for i in range(self.batch_size):
-                    self.p_We_q.append(tf.matmul(passage_outputs_unstack[i] ,We_q_unstacked[i]))
-                self.p_We_q = tf.concat(self.p_We_q,axis= -1)
-                self.p_We_q = tf.transpose(self.p_We_q ,[1,0])
+                self.p_We_q =  self.bilineaAtten(passage_outputs,final_query,"pre_end")
 
         print("Training state :{}".format(self.config.is_training))
         if self.config.is_training is False :
@@ -253,9 +246,8 @@ class model(object):
         tf.summary.scalar("sum_loss", self.cross_entropy_start +self.cross_entropy_end )
         #tf.summary.scalar("gradient_norm", gradient_norm)
 
-        tf.summary.histogram("Ws",Ws)
+
 
         #tf.summary.histogram("embeddings",embeddings)
 
-        tf.summary.histogram("We",We)
         self.summary_op = tf.summary.merge_all()
